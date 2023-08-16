cf5c0db487e55698666022915059bdef4bbd892a
Ryszard Napierała <cozajeden@gmail.com>
2023-08-16 18:11:51 +0200

    :hammer:

--- first_steps/urls.py
+++ first_steps/urls.py
@@ -5,12 +5,8 @@ app_name = 'first_steps'
 
 urlpatterns = [
     path('', views.name_list, name='name_list'),
-    path('name-page/<int:page>/', views.name_page, name='name_page'),
-    path('name-page/<int:page>/<str:name>/', views.name_page, name='name_page_filtered'),
+    path('name-page/', views.name_page, name='name_page'),
     path('list/', views.list, name='list'),
-    path('list/<int:page>/', views.list, name='list_paged'),
     path('page-numbers/', views.page_numbers, name='page_numbers'),
-    path('page-numbers/<str:name>', views.page_numbers, name='page_numbers_filtered'),
-    path('search-element/<int:page>/', views.search_element, name='search_element'),
-    path('search-element/<int:page>/<str:name>/', views.search_element, name='search_element_filtered'),
+    path('search-element/', views.search_element, name='search_element'),
 ]
--- first_steps/views.py
+++ first_steps/views.py
@@ -9,37 +9,59 @@ per_page = 10
 
 @require_http_methods(['GET'])
 def name_list(request):
-    return render(request, 'first_steps/name_list.html')
-
-@require_http_methods(['GET'])
-def search_element(request, page=1, name=''):
-    return render(request, 'first_steps/components/search_element.html', {
-        'page': page,
-        'name': name,
+    names = models.Name.objects.order_by('name')
+    return render(request, 'first_steps/name_list.html', {
+        'title': 'List of names',
+        'page': 1,
+        'name': '',
+        'objects': names[:per_page],
+        'pages': range(1, ceil(names.count() / per_page) + 1),
     })
 
 @require_http_methods(['GET'])
-def name_page(request, page=1, name=''):
+def search_element(request):
+    name = request.GET.get('name', '')
+    page = int(request.GET.get('page', 1))
+    return render(request, 'first_steps/components/search_element.html', {
+        'name': name,
+        'page': page,
+    })
+
+@require_http_methods(['GET'])
+def name_page(request):
+    name = request.GET.get('name', '')
+    page = int(request.GET.get('page', 1))
     names = models.Name.objects.order_by('name')
     if name: names = names.filter(name__icontains=name)
     page = min(page, ceil(names.count() / per_page))
     return render(request, 'first_steps/components/names.html', {
         'objects': names[(page - 1) * per_page:page * per_page],
-    })
-
-@require_http_methods(['GET'])
-def page_numbers(request, name=''):
-    names = models.Name.objects.all()
-    if name: names = names.filter(name__icontains=name)
-    return render(request, 'first_steps/components/page_numbers.html', {
-        'pages': range(1, ceil(names.count() / per_page) + 1),
+        'page': page,
         'name': name,
     })
 
 @require_http_methods(['GET'])
-def list(request, page=1):
+def page_numbers(request):
+    names = models.Name.objects.all()
     name = request.GET.get('name', '')
+    page = int(request.GET.get('page', 1))
+    if name: names = names.filter(name__icontains=name)
+    return render(request, 'first_steps/components/page_numbers.html', {
+        'pages': range(1, ceil(names.count() / per_page) + 1),
+        'name': name,
+        'page': page,
+    })
+
+@require_http_methods(['GET'])
+def list(request):
+    name = request.GET.get('name', '')
+    page = int(request.GET.get('page', 1))
+    names = models.Name.objects.order_by('name')
+    if name: names = names.filter(name__icontains=name)
+    page = min(page, ceil(names.count() / per_page))
     return render(request, 'first_steps/components/list.html', {
+        'pages': range(1, ceil(names.count() / per_page) + 1),
+        'objects': names[(page - 1) * per_page:page * per_page],
         'title': 'List of names',
         'page': page,
         'name': name,
