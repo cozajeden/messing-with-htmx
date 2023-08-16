0da464a8e5d839741fdb5a1bb93989c202452b9e
Ryszard Napierała <cozajeden@gmail.com>
2023-08-16 21:59:14 +0200

    :boom: calculating pagination, added create and delete

--- first_steps/urls.py
+++ first_steps/urls.py
@@ -5,8 +5,7 @@ app_name = 'first_steps'
 
 urlpatterns = [
     path('', views.name_list, name='name_list'),
-    path('name-page/', views.name_page, name='name_page'),
     path('list/', views.list, name='list'),
-    path('page-numbers/', views.page_numbers, name='page_numbers'),
-    path('search-element/', views.search_element, name='search_element'),
+    path('list/add', views.create, name='list_add'),
+    path('delete/<str:name>', views.delete, name='list_delete'),
 ]
--- first_steps/views.py
+++ first_steps/views.py
@@ -1,68 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.db.utils import IntegrityError
 from django.views.decorators.http import require_http_methods
+from django.db import transaction
 from . import models
 from math import ceil
+from typing import List
 import logging
 
 
 per_page = 10
 
+def calculate_pages(names, page: int) -> List[int]:
+    """Calculate pages for pagination."""
+    pages_count = ceil(names.count() / per_page)
+    page = max(page, 5)
+    first_page = min(page - 3, pages_count - 7)
+    last_page = max(min(pages_count, page + 4), 8)
+    pages = set(range(first_page, last_page))
+    pages.add(1)
+    pages.add(pages_count)
+    return sorted(pages)
+    
+
 @require_http_methods(['GET'])
 def name_list(request):
+    """Main view"""
     names = models.Name.objects.order_by('name')
     return render(request, 'first_steps/name_list.html', {
-        'title': 'List of names',
-        'page': 1,
-        'name': '',
+        'pages': calculate_pages(names, 1),
         'objects': names[:per_page],
-        'pages': range(1, ceil(names.count() / per_page) + 1),
-    })
-
-@require_http_methods(['GET'])
-def search_element(request):
-    name = request.GET.get('name', '')
-    page = int(request.GET.get('page', 1))
-    return render(request, 'first_steps/components/search_element.html', {
-        'name': name,
-        'page': page,
-    })
-
-@require_http_methods(['GET'])
-def name_page(request):
-    name = request.GET.get('name', '')
-    page = int(request.GET.get('page', 1))
-    names = models.Name.objects.order_by('name')
-    if name: names = names.filter(name__icontains=name)
-    page = min(page, ceil(names.count() / per_page))
-    return render(request, 'first_steps/components/names.html', {
-        'objects': names[(page - 1) * per_page:page * per_page],
-        'page': page,
-        'name': name,
-    })
-
-@require_http_methods(['GET'])
-def page_numbers(request):
-    names = models.Name.objects.all()
-    name = request.GET.get('name', '')
-    page = int(request.GET.get('page', 1))
-    if name: names = names.filter(name__icontains=name)
-    return render(request, 'first_steps/components/page_numbers.html', {
-        'pages': range(1, ceil(names.count() / per_page) + 1),
-        'name': name,
-        'page': page,
+        'page': 1,
     })
 
 @require_http_methods(['GET'])
 def list(request):
+    """List of names (HTML component)"""
     name = request.GET.get('name', '')
     page = int(request.GET.get('page', 1))
     names = models.Name.objects.order_by('name')
     if name: names = names.filter(name__icontains=name)
-    page = min(page, ceil(names.count() / per_page))
+    page = max(min(page, ceil(names.count() / per_page)), 1)
     return render(request, 'first_steps/components/list.html', {
-        'pages': range(1, ceil(names.count() / per_page) + 1),
         'objects': names[(page - 1) * per_page:page * per_page],
-        'title': 'List of names',
+        'pages': calculate_pages(names, page),
         'page': page,
         'name': name,
         })
+
+@require_http_methods(['POST'])
+def create(request):
+    name = request.POST.get('add-name', '')
+    try:
+        if name: models.Name.objects.create(name=name)
+    except IntegrityError:
+        logging.warning(f'Name {name} already exists')
+    return redirect(reverse('first_steps:list') + '?' + request.body.decode('utf-8'))
+
+@require_http_methods(['DELETE'])
+@transaction.atomic
+def delete(request, name):
+    models.Name.objects.filter(name=name).delete()
+    request.method = 'GET'
+    query_params = request.body.decode('utf-8').split('&')
+    request.GET = {param.split('=')[0]: param.split('=')[1] for param in query_params if param}
+    return list(request)
